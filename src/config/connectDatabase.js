240c1d8e9e80a4bae7529ace4111c2bcb0805b85
AhKhoi202 <khoianh202@gmail.com>
2023-11-21 17:12:15 +0700

    deloy server

--- src/config/connectDatabase.js
+++ src/config/connectDatabase.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 
 
-const sequelize = new Sequelize('partner', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 const connectDatabase = async () => {
